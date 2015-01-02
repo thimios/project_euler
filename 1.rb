b70f36d37b833a123788c8c4902d811a544170b4
Thimios Dimopulos <thimios@wantedpixel.com>
2015-01-02 18:47:05 +0200

    updated formatting

--- 1.rb
+++ 1.rb
@@ -4,7 +4,9 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 =end
+
 require "test/unit"
+
 include Test::Unit::Assertions
 
 assert (1...1000).select{|x|x % 3 == 0 || x % 5 == 0}.reduce(:+) == 233168
